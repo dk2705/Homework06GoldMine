9c4db0e3d1b019d8c972533a3515eb6657235247
Dmytro Kotliar <dn270571kdb@gmail.com>
2021-06-15 14:16:10 +0300

    my version

--- src/main/java/goldmine/Counter.java
+++ src/main/java/goldmine/Counter.java
@@ -1,5 +1,42 @@
 package goldmine;
 
 public class Counter extends Thread{
+    
+    private Worker worker1;
+    private Worker worker2;
+    private Worker worker3;
+    private Mine mine;
+
+    
+    public Counter(Worker worker1, Worker worker2, Worker worker3, Mine mine) {
+        this.worker1 = worker1;
+        this.worker2 = worker2;
+        this.worker3 = worker3;
+        this.mine = mine;
+    }
+    
+    @Override
+    public void run() {
+        long amountOfGoldMine = 50;
+        long amountOfGoldWorker1 = 0;
+        long amountOfGoldWorker2 = 0;
+        long amountOfGoldWorker3 = 0;
+        while(amountOfGoldMine >= 3){
+            amountOfGoldMine = mine.getAmountOfGold();
+            amountOfGoldWorker1 = worker1.getAmountOfGold();
+            amountOfGoldWorker2 = worker2.getAmountOfGold();
+            amountOfGoldWorker3 = worker3.getAmountOfGold();
+            try{
+                Thread.sleep(1000);
+                System.out.println(   worker1.getWorkerName() + ": " + amountOfGoldWorker1 + " g; "
+                                    + worker2.getWorkerName() + ": " + amountOfGoldWorker2 + " g; "
+                                    + worker3.getWorkerName() + ": " + amountOfGoldWorker3 + " g; "
+                                    + "left in mine: " + amountOfGoldMine + " g;"
+                                  );
+            }catch(InterruptedException e){
+                System.out.println("Service Error");
+            }
+        }
+    }
 
 }
--- src/main/java/goldmine/Mine.java
+++ src/main/java/goldmine/Mine.java
@@ -2,4 +2,15 @@ package goldmine;
 
 public class Mine {
     
+    private int amountOfGold = 50; //1000 - to long
+    
+    public synchronized void getPortionOfGold(Worker worker) throws InterruptedException {
+        Thread.sleep(1000);
+        amountOfGold -= 3;
+        worker.increaseAmountOfGold(3);
+    }
+    
+    public synchronized long getAmountOfGold() {
+        return amountOfGold;
+    }
 }
